1ade0ec0dac162376c46722053e8c3bc23466765
vamsi krishna <r18cs273@cit.reva.edu.in>
2020-11-05 12:43:34 +0530

    firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'authentication_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MedRem',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Container(child: Text("MedRem")),
-    );
+    return MultiProvider(
+        providers: [
+          Provider<AuthenticationService>(
+            create: (_) => AuthenticationService(FirebaseAuth.instance),
+          ),
+          StreamProvider(
+            create: (context) =>
+                context.read<AuthenticationService>().authStateChanges,
+          )
+        ],
+        child: MaterialApp(
+          home: AuthenticationWrapper(),
+        ));
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return App();
+    }
+    return Login();
   }
 }
